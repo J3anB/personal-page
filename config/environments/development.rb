9cc09e400376b9bbfcec1ead6e4f8dc047da2894
Jean Berti <berti.jean@gmail.com>
2020-07-28 16:47:54 +0200

    Action Mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,9 +32,20 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  # Mailer SMTP configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'og.the.smooth.cat',
+      password:             'wppwjhfhbzdtbfhl',
+      authentication:       'plain',
+      enable_starttls_auto: true }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
