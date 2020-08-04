051199e9fdd3e67350f1515625dd169d74888092
Jean Berti <berti.jean@gmail.com>
2020-08-04 11:21:30 +0200

    correcting admin reply controller

--- app/controllers/admin_reply_controller.rb
+++ app/controllers/admin_reply_controller.rb
@@ -3,8 +3,8 @@ class AdminReplyController < ApplicationController
   def create
     @contact = Contact.find(params[:id])
     @reply = @contact.create_admin_reply(reply_params)
-    redirect_to contact_path(@contact)
     ReplyMailer.with(contact: @contact).reply_email.deliver_now
+    redirect_to contact_path(@contact)
   end
 
 
