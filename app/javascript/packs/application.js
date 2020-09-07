d787fa1db8b99200d7f983fe065aab7beff4ce1a
Jean Berti <berti.jean@gmail.com>
2020-09-07 15:05:23 +0200

    application.js
    deleted room.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ require("bootstrap")
 require("jquery")
 require("custom/toggle.js")
 require("custom/weather.js")
-require("custom/room.js")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
